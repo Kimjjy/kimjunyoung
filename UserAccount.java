9c6c963fd678186c6318f1cc7584a7827e260b75
박소원 <ssowon3@gmail.com>
2022-07-15 11:26:24 +0900

    UserActivity

---
+++ UserAccount.java
@@ -0,0 +1,23 @@
+package com.cookandroid.kneelingbus;
+
+import android.provider.ContactsContract;
+
+public class UserAccount {
+    private String idToken;
+    private  String Email;
+    private String password;
+
+    public UserAccount() { }
+
+    public String getIdToken() { return idToken;}
+    public void setIdToken(String idToken){this.idToken = idToken;}
+
+    public String getEmail() { return Email ;}
+    public void setEmailId(String  email){this. Email = email;}
+
+    public String getPassword() { return password; };
+    public void setPassword(String password){this.password= password;}
+
+
+
+}
